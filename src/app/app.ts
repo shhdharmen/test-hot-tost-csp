e4082efc7a8671835eb24c02c9d80f94f378636c
Dharmen Shah <shhdharmen@gmail.com>
2025-06-02 13:19:29 +0530

    feat: use styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,6 +12,16 @@ export class App {
   protected toast = inject(HotToastService);
 
   showToast() {
-    this.toast.success('Hello, world!');
+    this.toast.success('Look at my styles', {
+      style: {
+        border: '1px solid #713200',
+        padding: '16px',
+        color: '#713200',
+      },
+      iconTheme: {
+        primary: '#713200',
+        secondary: '#FFFAEE',
+      },
+    });
   }
 }
